8254964f6bae4d407f9c7d7b77f92ee8d5ec8845
Zylqor <zacktarnark@gmail.com>
2025-12-08 19:20:31 +0300

    Add homepage with links to Sign Up and Login

--- pages/index.js
+++ pages/index.js
@@ -1,1 +1,13 @@
+import Link from "next/link";
 
+export default function Home() {
+  return (
+    <div className="min-h-screen flex flex-col items-center justify-center bg-black text-white">
+      <h1 className="text-5xl font-bold mb-6">Welcome to QYVTHRO</h1>
+      <div className="flex space-x-4">
+        <Link href="/signup" className="px-6 py-3 bg-red-500 rounded-lg">Sign Up</Link>
+        <Link href="/login" className="px-6 py-3 bg-gray-700 rounded-lg">Login</Link>
+      </div>
+    </div>
+  );
+    }
