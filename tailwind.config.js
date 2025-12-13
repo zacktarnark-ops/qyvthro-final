fb50c78c43e5510b15e5f26795c8e765a002ec0c
Zylqor <zacktarnark@gmail.com>
2025-12-13 19:35:14 +0300

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,7 +6,24 @@ module.exports = {
     "./utils/**/*.{js,jsx}"
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        gold: {
+          50: '#FFF8DC',
+          100: '#FFEDB3',
+          200: '#FFE066',
+          300: '#FFD700',
+          400: '#D4AF37',
+          500: '#B8860B',
+        },
+        black: {
+          900: '#000000',
+          800: '#1A1A1A',
+          700: '#2D2D2D',
+          600: '#404040',
+        }
+      }
+    },
   },
   plugins: [],
 }
